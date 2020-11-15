3fc0600df177c27acfb79870298bf2d36494a45a
Max <maxabaum@gmail.com>
2020-11-15 16:33:51 -0500

    game files

--- egg_invaders.py
+++ egg_invaders.py
@@ -28,8 +28,8 @@ class EggInvaders:
         
         pygame.display.set_caption("Egg Invaders")
 
-        #make an instance of the owl w/ required argument of EggInvaders
-        #This gives access the Owl access t o game resources
+        #make an instance of the owl w/ required argument which is the instance EggInvaders
+        #This gives access the Owl access to game resources
         self.owl = Owl(self)
 
         #set the background color using Settings module
@@ -40,19 +40,27 @@ class EggInvaders:
         """Start the main loop for the game"""
 
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            #Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+            self._check_events()
+            # Redraw the screen during each pass through the loop.
+            self._update_screen()
 
-            #draws the owl on screen
-            self.owl.blitme()
+#helper methods does work inside a class but isn't called through an instance.
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            #Make the most recently drawn screen visible.
-            pygame.display.flip()
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        #Redraw the screen during each pass through the loop.
+        self.screen.fill(self.bg_color)
+
+        #draws the owl on screen
+        self.owl.blitme()
+
+        #Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ class Settings:
         #screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 0, 255)
+        self.bg_color = (255, 255, 255)
         
