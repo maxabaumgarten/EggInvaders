17a7f1c3d9e3a9ab9294325f287f72764da63213
Max <maxabaum@gmail.com>
2020-11-15 13:04:10 -0500

    code updates

--- egg_invaders.py
+++ egg_invaders.py
@@ -13,6 +13,9 @@ class EggInvaders:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Egg Invaders")
+
+        #set the background color.
+        self.bg_color = (0, 0, 255)
     
     def run_game(self):
         """Start the main loop for the game"""
@@ -23,6 +26,9 @@ class EggInvaders:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            #Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+
             #Make the most recently drawn screen visible.
             pygame.display.flip()
 
